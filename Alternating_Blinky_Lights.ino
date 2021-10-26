945093c7109ecc6bd5b5c0816a3b23dac1f575a5
Daryll Fortunado <daryllfortunado@gmail.com>
2021-10-26 20:11:24 +0800

    Initial Commit

---
+++ Alternating_Blinky_Lights.ino
@@ -0,0 +1,40 @@
+void setup() {
+  // put your setup code here, to run once:
+    pinMode(2, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    digitalWrite(2, HIGH);
+    delay(500);
+    digitalWrite(2,  LOW);
+    delay(500);
+    digitalWrite(4, HIGH);
+    delay(500);
+    digitalWrite(4, LOW);
+    delay(500);
+    digitalWrite(6, HIGH);
+    delay(500);
+    digitalWrite(6, LOW);
+    delay(500);
+    digitalWrite(9, HIGH);
+    delay(500);
+    digitalWrite(9, LOW);
+    delay(500);
+    digitalWrite(11, HIGH);
+    delay(500);
+    digitalWrite(11, LOW);
+    delay(500);
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
+  
+    
+}
